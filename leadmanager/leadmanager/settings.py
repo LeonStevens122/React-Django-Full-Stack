c22d017dbfdd24f996e9ad8e152eb3939b3ec2c9
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-08-17 10:33:52 +0200

    fixing static root

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -131,3 +131,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
