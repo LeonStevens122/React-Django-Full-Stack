43329a79bc76c5c8aaa8ff076bb1388a21cd6e26
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-08-17 22:16:42 +0200

    WSGI Settings

--- leadmanager/leadmanager/wsgi.py
+++ leadmanager/leadmanager/wsgi.py
@@ -12,6 +12,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leadmanager.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "leadmanager.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "leadmanager")
 application = get_wsgi_application()
