241dbfa33d6a6bbb04c9584f33467502b843d3b2
Leon Stevens <32592070+LeonStevens122@users.noreply.github.com>
2020-08-17 13:07:52 +0200

    procfile updated

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
+# url patterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
